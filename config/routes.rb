aea8898abfbedd6f4f15d215eca4e07e221d1d43
vinhduong0906 <vinhduong0906@gmail.com>
2020-04-26 12:31:51 +0900

    Update to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :articles
-  root 'articles#index'
-  
+  root 'pages#home'
+  get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
